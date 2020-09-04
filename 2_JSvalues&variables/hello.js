cc14fd9a7e1a7211b5a1eab409931b47f886b332
Iron Man <singhmrinal19@gmail.com>
2020-09-04 11:31:34 +0530

    commit #2

--- 2_JSvalues&variables/hello.js
+++ 2_JSvalues&variables/hello.js
@@ -1,1 +1,8 @@
-console.log("hello world");
+console.log(0==false);//true
+console.log(0===false);//false
+console.log(0=="");//true
+console.log(0==="");//false
+console.log(typeof "abcd");
+let a="   ddddd   dfd    ";
+a=a.trim();
+console.log(a);

99cdcd36f27bf15c4019e67c231d6e057632d41e
Iron Man <singhmrinal19@gmail.com>
2020-09-06 20:51:56 +0530

    adding app.js

---
+++ 2_JSvalues&variables/app.js
@@ -0,0 +1,14 @@
+let password="11 22 asdc";
+
+
+if(password.length>=6){
+    if(password.indexOf(" ")===-1){
+        console.log("password accepted")
+    }
+    else{
+        console.log("spaces not allowed")
+    }
+}
+else{
+    console.log("password not accepted")
+}
--- 2_JSvalues&variables/hello.js
+++ 2_JSvalues&variables/hello.js
@@ -1,3 +1,4 @@
+//exception
 console.log(0==false);//true
 console.log(0===false);//false
 console.log(0=="");//true

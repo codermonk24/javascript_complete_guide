593ec04d39f2de6914fad6b96fc578be459ec06a
Iron Man <singhmrinal19@gmail.com>
2020-09-06 20:58:26 +0530

    code formatter changes

--- 2_JSvalues&variables/hello.js
+++ 2_JSvalues&variables/hello.js
@@ -1,9 +1,9 @@
 //exception
-console.log(0==false);//true
-console.log(0===false);//false
-console.log(0=="");//true
-console.log(0==="");//false
-console.log(typeof "abcd");
-let a="   ddddd   dfd    ";
-a=a.trim();
+console.log(0 == false); //true
+console.log(0 === false); //false
+console.log(0 == ''); //true
+console.log(0 === ''); //false
+console.log(typeof 'abcd');
+let a = '   ddddd   dfd    ';
+a = a.trim();
 console.log(a);

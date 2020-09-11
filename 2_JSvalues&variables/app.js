f91c3c68b28151fcde2cffc6610d45c4dbb61f5d
Mrinal Singh <singhmrinal19@gmail.com>
2020-09-11 20:00:32 +0530

    commit #3

--- 2_JSvalues&variables/app.js
+++ 2_JSvalues&variables/app.js
@@ -1,11 +1,11 @@
-let password = '11 22 asdc';
+let password = "1122asdc";
 
 if (password.length >= 6) {
-	if (password.indexOf(' ') === -1) {
-		console.log('password accepted');
-	} else {
-		console.log('spaces not allowed');
-	}
+  if (password.indexOf("  ") === -1) {
+    console.log("password accepted");
+  } else {
+    console.log("spaces not allowed");
+  }
 } else {
-	console.log('password not accepted');
+  console.log("password not accepted");
 }

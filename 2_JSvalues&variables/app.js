f07a473795e275bfb450de1679aa9e4ac2bd7765
Mrinal Singh <singhmrinal19@gmail.com>
2020-09-11 20:09:15 +0530

    commit #4

--- 2_JSvalues&variables/app.js
+++ 2_JSvalues&variables/app.js
@@ -9,3 +9,27 @@ if (password.length >= 6) {
 } else {
   console.log("password not accepted");
 }
+
+//switch ---------------
+
+let num = 1;
+
+switch (num) {
+  case 1:
+    console.log("1");
+    break;
+
+  default:
+    break;
+}
+
+//ternary operator-----------
+
+let color = "Green";
+
+color === "green" ? console.log("accepted") : console.log("rejected");
+let status = "offline";
+
+let colour = status === "offline" ? "red" : "green";
+
+console.log(colour);
